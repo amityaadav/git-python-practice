5a4ef1c91b3ec64a5b73664f5542bfc4cd73476b
magnetar-coder <yadav.amit.in@gmail.com>
2025-12-13 16:07:52 -0500

    branch cleanup

---
+++ workout_tracker.py
@@ -0,0 +1,21 @@
+# Strings - text
+workout_name = "Legs"
+
+# Numbers - integers and floats
+reps = 6
+weight = 135.0
+
+# Print outputs to the console
+print(workout_name)
+print(weight)
+
+# f-strings let you mix text and variables
+print(f"Lifted {weight} lbs for {reps} reps")
+
+# input() prompts the user and returns a string
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+#add reps and weight as int
+reps = int(input("How many reps: "))
+weight = int(input("How much weight: "))
