a8b5a6c49e62599b68ab6d36fbfecae5bf75e6bb
magnetar-coder <yadav.amit.in@gmail.com>
2025-12-13 16:30:58 -0500

    day 1 drill complete

--- day1_drill.py
+++ day1_drill.py
@@ -1,2 +1,4 @@
 name = input("What\'s your name: ")
-fav_exercise = 
+fav_exercise = input("What\'s your favorite exercise: ")
+max_weight = int(input("How much weight can you lift in this exercise: "))
+print(f"{name}'s favorite exercise is {fav_exercise}, and he can lift {max_weight} lbs")
--- workout_tracker.py
+++ workout_tracker.py
@@ -1,21 +1,9 @@
-# Strings - text
-workout_name = "Legs"
-
-# Numbers - integers and floats
-reps = 6
-weight = 135.0
-
-# Print outputs to the console
-print(workout_name)
-print(weight)
-
-# f-strings let you mix text and variables
-print(f"Lifted {weight} lbs for {reps} reps")
-
-# input() prompts the user and returns a string
-name = input("Enter your name: ")
-print(f"Hello, {name}")
-
-#add reps and weight as int
+workout_name = input("What workout are we doing today: ")
+exercise = input("What exercise: ")
+weight = float(input("How much weight: "))
 reps = int(input("How many reps: "))
-weight = int(input("How much weight: "))
+print("---Workout Entry---")
+print(f"Workout: {workout_name}")
+print(f"Exercise: {exercise}")
+print(f"Weight: {weight}")
+print(f"Reps: {reps}")
