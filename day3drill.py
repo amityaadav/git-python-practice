ca9bd1d91c6c67c2caadbca0e9c892a75710e22d
magnetar-coder <yadav.amit.in@gmail.com>
2025-12-18 18:07:41 -0500

    day 3 changes

---
+++ day3drill.py
@@ -0,0 +1,10 @@
+set1Weight=(135)
+set1Rep=8
+set2Weight=155
+set2Rep=6
+set3Weight=175
+set3Rep=4
+avg = (set1Weight+set2Weight+set3Weight)/3
+total_volume = (set1Weight * set1Rep)+(set2Weight * set2Rep)+(set3Weight * set3Rep)
+print(f"Average weight lifted was {avg}")
+print(f"Total Volume: {total_volume} lbs")
