5d3b76b440ec4d013c739e5effe7f0dd16bd6b72
magnetar-coder <yadav.amit.in@gmail.com>
2026-01-02 07:42:50 -0500

    create functions for input data and total volume

--- workout_tracker.py
+++ workout_tracker.py
@@ -10,27 +10,25 @@ if not exercise_name:
 
 # User Input: Weight and Reps
 set_count =  int(input("How many sets are you doing today "))
-count = 1
+
+# input function
+def get_set_data(set_number):
+    weight_input = int(input(f"Set {set_number} weight in lbs: "))
+    rep_input = int(input(f"Set {set_number} reps: "))
+    return weight_input, rep_input
+
+# total volume function
+def calc_total_volume(weights, reps):
+        return (sum(w * r for w,r in zip(weights, reps)))
+
+# call input function
 weights = []
 reps = []
-while count <= set_count:
-    try:
-        input_weight = int(input(f"Set {count} weight in lbs: "))
-        input_rep = int(input(f"Set {count} reps:"))
-        if input_weight <= 0 or input_rep <=0:
-            print("Weights must be positive value")
-            continue
-        weights.append(input_weight)
-        reps.append(input_rep)
-        count += 1
-    except ValueError:
-        print("Please enter a valid number")
+for i in range(set_count):
+    w, r = get_set_data(i + 1)  # returns two values
+    weights.append(w)
+    reps.append(r)
 
-# Print lists  
-for i, (w,r) in enumerate(zip(weights, reps)):
-    print(f"Set {i+1}: with {w} lbs for {r} reps")
-    print(f"Volume for set {i+1} is {w * r}")
-
-# Total Volume
-total_volume = (sum(w * r for w, r in zip(weights, reps)))
-print(f"Total Volume of your workout is: {total_volume}")
+# call total vol function
+total = calc_total_volume(weights, reps)
+print(f"Total volume: {total}")
